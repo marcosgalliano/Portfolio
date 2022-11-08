e24acea406f889e70b41b620cc121b382ee5304e
marcos galliano <marquitosgalliano@hotmail.com>
2022-11-08 09:39:39 -0300

    portfolio aboutMe

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 window.addEventListener("scroll", () => {
     //SKILLS
-    let animacion = document.getElementById("skillsx");
+    let animacion = document.getElementById("skills");
     let posicionObj1 = animacion.getBoundingClientRect().top;
     //EDUCATION
-    let animacion2 = document.getElementById("educationx");
+    let animacion2 = document.getElementById("education");
     let posicionObj2 = animacion2.getBoundingClientRect().top;
+    //CONTACT
+    let animacion3 = document.getElementById("contact");
+    let posicionObj3 = animacion3.getBoundingClientRect().top;
     //PANTALLA
     let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
-        animacion.style.animation = 'efectoh1X2 1.2s ease-out'
+        animacion.style.animation = 'efecto 2s ease-out'
     }
 
     if(posicionObj2 < tamañoDePantalla){
-        animacion2.style.animation = 'efecto 2.5s ease-out'
+        animacion2.style.animation = 'efecto 2s ease-out'
+    }
+
+    if(posicionObj3 < tamañoDePantalla){
+        animacion3.style.animation = 'efecto 2s ease-out'
     }
 })
